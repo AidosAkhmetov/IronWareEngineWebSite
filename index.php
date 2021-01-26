ed0310b7e665a99305e2ab1ec34f69c6e1799244
Aidos <aidos1akh@gmail.com>
2021-01-26 23:48:24 +0600

    adding to main page navigation

--- index.php
+++ index.php
@@ -11,14 +11,24 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-	<title>My website</title>
+	<title>Main</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-	<a href="logout.php">Logout</a>
-	<h1>This is the index page</h1>
-
-	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+    <ul class="nav">
+  		<li><a href="logout.php">Logout</a></li>    
+		<li><?php echo $user_data['user_name']; ?></li>
+		
+  		<li><a href="#">Forum</a></li>
+  		<li><a href="#">Documentation</a></li>
+  		
+  		<li><a href="#">About Us</a></li>
+  		<li><a href="#">Home</a></li>
+  		
+	</ul> 
+	
+	
+	
 </body>
 </html>
