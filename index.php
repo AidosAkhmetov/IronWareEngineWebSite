817bb70d417268cb2517bf7c8817118cecdf7a2c
Aidos <aidos1akh@gmail.com>
2021-03-16 10:52:11 +0600

    .

--- index.php
+++ index.php
@@ -32,85 +32,24 @@ session_start();
       		<a href="logout.php">Logout</a>
 
     	</div>
- 
-       
-
-
   		</li>
-<<<<<<< HEAD
   		<li style="float: right"><a href="test.exe" download="test" id="download">Download</a></li>
   		
 	</ul>	
 
   <!---<div class="slider">
-=======
-  		<!--<li style="float: right"><a  id="download" href="#">Download</a></li-->
+  		<li style="float: right"><a  id="download" href="#">Download</a></li
   		<li style="float: right"><a href="test.exe" download="test" id="download">Download</a></li>
 	</ul>	
-
+-->
   
-<script>
->>>>>>> temp
 
-	var images = ['img/a.jpg', 'img/b.jpg', 'img/c.jpg', 'img/d.jpg', 'img/e.jpg'];
 
-<<<<<<< HEAD
-  </div>
-  -->
 
- <footer>
-        <div class="container">
-            <div class="footer-content">
-                <div class="items">
-                    <a href="#" class="logo">Skrapp.io</a>  
-                    <p> Skrapp.io is an Email Finder for B2B sales and email marketing. We are collecting and crafting public data to provide an
-                    advanced prospecting tool to help professionals launch performant email outreach campaigns.</p>
-                    <div class="social">
-                        <a href="#" class="fo"><i class="fa fa-facebook"></i></a>
-                        <a href="#" class="fo"><i class="fa fa-twitter"></i></a>
-                        <a href="#" class="fo"><i class="fa fa-youtube-play"></i></a>
-                        <a href="#" class="fo"><i class="fa fa-github"></i></a>
-                    </div>
-                </div>
-                <div class="items">
-                    <h3> Product</h3>
-                    <ul>
-                        <li class="lim"><a class="fo" href="#">Chrome Extension</a></li>
-                        <li class="lim"><a  class="fo" href="#">Email Finder</a></li>
-                        <li class="lim"><a  class="fo" href="#">Bulk Email Finder</a></li>
-                        <li class="lim"><a class="fo" href="#">Companies</a></li>
-                        <li class="lim"><a class="fo" href="#">Index</a></li>
-                        <li class="lim"><a class="fo" href="#">API</a></li>
-                        <li class="lim"><a class="fo" href="#">Pricing</a></li>
-                    </ul>                          
-                </div>
+ 
 
-                <div class="items">
-                    <h3>Legal</h3>
-                <ul>
-                    <li class="lim"><a href="#">Terms</a></li>
-                    <li class="lim"><a href="#">Privacy Policy</a></li>
-                    <li class="lim"><a href="#">Cookie Policy</a></li>
-                    <li class="lim"><a href="#">GDPR Compliance</a></li>
-                </ul>
-                </div>
-                <div class="items">
-                    <h3>Support</h3>
-                    <ul>
-                        <li class="lim"><a href="#">Knowledge Base</a></li>
-                        <li class="lim"><a href="#">Video Tutorials</a></li>
-                    </ul>
-                </div>
-
-            </div>
-            <hr>
-            <p class="end">Copyright © 2020 by Skrapp.io. All rights reserved.</p>
-        </div>
-    </footer>
 <script type="text/javascript">
 	var images = ['img/far.jpg','img/fifa.jpg'];
-=======
->>>>>>> temp
     var x = 0;
 
     var imgs = document.getElementById('img');
