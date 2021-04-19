291d0769d0220b6f2b3362bb6ce36ebb1fbf20bd
Aidos <aidos1akh@gmail.com>
2021-04-19 06:49:55 +0600

    ajax question form

---
+++ config/config.php
@@ -0,0 +1,71 @@
+<?php
+	
+	class Database 
+	{
+		private $servername = "localhost";
+		private $username   = "root";
+		private $password   = "";
+		private $dbname = "ironware";
+		public $con;
+		public $customerTable = "itemss";
+
+		public function __construct()
+		{
+			try {
+				$this->con = new mysqli($this->servername, $this->username, $this->password, $this->dbname);	
+			} catch (Exception $e) {
+				echo $e->getMessage();
+			}
+		}
+
+		// Insert customer data into customer table
+		public function insertRecond($title, $body, $dob)
+		{
+			$sql = "INSERT INTO $this->customerTable (title, body, dob) VALUES('$title','$body','$dob')";
+			$query = $this->con->query($sql);
+			if ($query) {
+				return true;
+			}else{
+				return false;
+			}
+		}
+
+		// Fetch customer records for show listing
+		public function displayRecord()
+		{
+			$sql = "SELECT * FROM $this->customerTable";
+			$query = $this->con->query($sql);
+			$data = array();
+			if ($query->num_rows > 0) {
+				while ($row = $query->fetch_assoc()) {
+					$data[] = $row;
+				}
+				return $data;
+			}else{
+				return false;
+			}
+		}
+
+		// Fetch single data for edit from customer table
+		public function getRecordById($id)
+		{
+			$query = "SELECT * FROM $this->customerTable WHERE id = '$id'";
+			$result = $this->con->query($query);
+			if ($result->num_rows > 0) {
+				$row = $result->fetch_assoc();
+				return $row;
+			}else{
+				return false;
+			}
+		}
+
+
+		public function totalRowCount(){
+			$sql = "SELECT * FROM $this->customerTable";
+			$query = $this->con->query($sql);
+			$rowCount = $query->num_rows;
+			return $rowCount;
+		}
+
+	}
+?>
